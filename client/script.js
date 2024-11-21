9d3ef95771cec6d67c79649f2dc59e5ecc21d7ba
nickpinecone <nick.pineconefir@gmail.com>
2024-11-21 23:24:41 +0500

    Start adding ui

---
+++ client/script.js
@@ -0,0 +1,13 @@
+import "@microsoft/signalr";
+
+
+// var hub = new signalR.HubConnectionBuilder()
+// .withUrl("http")
+// export const signal = new signalR.HubConnectionBuilder()
+//     .withUrl("http://localhost:5000/signal", {
+//         transport: signalR.HttpTransportType.ServerSentEvents,
+//         withCredentials: true,
+//     })
+//     .build();
+//
+// signal.start();
