b5d5337a1b95da715c0f8003355f6e2199e492d4
nickpinecone <nick.pineconefir@gmail.com>
2024-11-25 18:01:21 +0500

    Generate voice files with tts

--- server/Program.cs
+++ server/Program.cs
@@ -3,6 +3,7 @@ using System.ClientModel;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using NetCoreAudio;
 using OllamaSharp;
 using OpenAI;
 using OpenAI.Audio;
@@ -17,6 +18,8 @@ public static class Program
         var app = builder.Build();
         var task = app.RunAsync();
 
+        var player = new Player();
+
         var uri = new Uri("http://localhost:11434");
         var ollama = new OllamaApiClient(uri);
         ollama.SelectedModel = "saiga-llama3";
@@ -25,11 +28,22 @@ public static class Program
             Endpoint = new Uri("http://localhost:8080/v1/"),
         });
         var whisper = openai.GetAudioClient("whisper-small-ru");
+        var tts = openai.GetAudioClient("ruslan.onnx");
 
         var path = Path.Combine(Directory.GetCurrentDirectory(), "output", "bak.mic.wav");
         var text = await whisper.TranscribeAudioAsync(path);
 
         Console.WriteLine(text.Value.Text);
+
+        var data = await tts.GenerateSpeechAsync(text.Value.Text, GeneratedSpeechVoice.Echo,
+                                                 new SpeechGenerationOptions() {
+                                                     ResponseFormat = GeneratedSpeechFormat.Wav,
+                                                 });
+        path = Path.Combine(Directory.GetCurrentDirectory(), "output", "test.wav");
+        BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create));
+        writer.Write(data.Value.ToArray());
+
+        await player.Play(path);
 
         await task;
     }
